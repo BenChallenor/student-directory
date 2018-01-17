77612e67c042fa54e1381895cd6468cfe0fc033b
Ben <ben.challenor@btopenworld.com>
2018-01-17 15:11:52 +0000

    Added more information

--- directory.rb
+++ directory.rb
@@ -4,21 +4,28 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    puts "What is their country of birth?"
+    cob = gets.chomp
+    puts "What is their height?"
+    height = gets.chomp
+    puts "What are their hobbies?"
+    hobbie = gets.chomp
+    students << {name: name, cob: cob, height: height, hobbie: hobbie, cohort: :november}
     puts "Now we have #{students.count} students"
     name= gets.chomp
   end
   students
 end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print(students)
-  x = 0
-  while x < students.length do
-    puts "#{students[x][:name]} (#{students[x][:cohort]}) cohort"
-    x += 1
+  students.each do |student|
+    puts "#{student[:name]}, C.O.B: #{student[:cob]}, Height: #{student[:height]}, hobbies: #{student[:hobbie]} (#{student[:cohort]}) cohort"
+
   end
 end
 def print_footer(names)
