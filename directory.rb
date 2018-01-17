725dccaed7f5651c945f9fd2d47a83a3054fc630
Ben <ben.challenor@btopenworld.com>
2018-01-17 14:03:09 +0000

    Print only students whose name begins with S

--- directory.rb
+++ directory.rb
@@ -16,7 +16,9 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
+    if student[:name].start_with? "S"
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
   end
 end
 def print_footer(names)
