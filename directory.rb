089e81a54d3b17bd65a8a48bc7b53d00ac8fe995
Ben <ben.challenor@btopenworld.com>
2018-01-19 13:04:54 +0000

    Taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -48,8 +48,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -57,11 +57,23 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesnt exist."
+    exit
+  end
+end
+
 def input_students
   puts "Please enter the names of the students".center(50)
   puts "To finish, just hit return twice".center(50)
   cohorts = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     puts "What cohort?".center(50)
   cohort = gets.chomp.capitalize.to_sym
@@ -93,7 +105,7 @@ def input_students
   else
     puts "Now we have #{@students.count} students".center(50)
     puts "Name:".center(50)
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -136,4 +148,5 @@ def print_footer
 end
 end
 
+try_load_students
 interactive_menu
